e35d82e7d1673ef1f8bde21878bd70d98ca63adb
Riso1 <93641234+Riso1@users.noreply.github.com>
2026-06-14 13:00:17 +0600

    Подключен готовый frontend

--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,6 @@
-from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404
 from django.db.models import Count, Sum
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -21,176 +21,170 @@ from .models import (
     Contract,
     Customer,
     Lead,
-    Service
+    Service,
 )
 
 
 class ServiceListView(ListView):
     model = Service
-    template_name = 'crm/service_list.html'
-    context_object_name = 'services'
+    template_name = 'products/products-list.html'
+    context_object_name = 'products'
 
 
 class ServiceDetailView(DetailView):
     model = Service
-    template_name = 'crm/service_detail.html'
+    template_name = 'products/products-detail.html'
     context_object_name = 'service'
 
 
 class ServiceCreateView(CreateView):
     model = Service
     form_class = ServiceForm
-    template_name = 'crm/service_form.html'
+    template_name = 'products/products-create.html'
 
 
 class ServiceUpdateView(UpdateView):
     model = Service
     form_class = ServiceForm
-    template_name = 'crm/service_form.html'
+    template_name = 'products/products-edit.html'
 
 
 class ServiceDeleteView(DeleteView):
     model = Service
-    template_name = 'crm/service_confirm_delete.html'
+    template_name = 'products/products-delete.html'
     success_url = reverse_lazy('crm:service_list')
 
 
 class AdvertisingCampaignListView(ListView):
     model = AdvertisingCampaign
-    template_name = 'crm/campaign_list.html'
-    context_object_name = 'campaigns'
+    template_name = 'ads/ads-list.html'
+    context_object_name = 'ads'
 
 
 class AdvertisingCampaignDetailView(DetailView):
     model = AdvertisingCampaign
-    template_name = 'crm/campaign_detail.html'
+    template_name = 'ads/ads-detail.html'
     context_object_name = 'campaign'
 
 
 class AdvertisingCampaignCreateView(CreateView):
     model = AdvertisingCampaign
     form_class = AdvertisingCampaignForm
-    template_name = 'crm/campaign_form.html'
+    template_name = 'ads/ads-create.html'
 
 
 class AdvertisingCampaignUpdateView(UpdateView):
     model = AdvertisingCampaign
     form_class = AdvertisingCampaignForm
-    template_name = 'crm/campaign_form.html'
+    template_name = 'ads/ads-edit.html'
 
 
 class AdvertisingCampaignDeleteView(DeleteView):
     model = AdvertisingCampaign
-    template_name = 'crm/campaign_confirm_delete.html'
+    template_name = 'ads/ads-delete.html'
     success_url = reverse_lazy('crm:campaign_list')
 
 
 class LeadListView(ListView):
     model = Lead
-    template_name = 'crm/lead_list.html'
+    template_name = 'leads/leads-list.html'
     context_object_name = 'leads'
 
 
 class LeadDetailView(DetailView):
     model = Lead
-    template_name = 'crm/lead_detail.html'
+    template_name = 'leads/leads-detail.html'
     context_object_name = 'lead'
 
 
 class LeadCreateView(CreateView):
     model = Lead
     form_class = LeadForm
-    template_name = 'crm/lead_form.html'
+    template_name = 'leads/leads-create.html'
 
 
 class LeadUpdateView(UpdateView):
     model = Lead
     form_class = LeadForm
-    template_name = 'crm/lead_form.html'
+    template_name = 'leads/leads-edit.html'
 
 
 class LeadDeleteView(DeleteView):
     model = Lead
-    template_name = 'crm/lead_confirm_delete.html'
+    template_name = 'leads/leads-delete.html'
     success_url = reverse_lazy('crm:lead_list')
 
 
 class ContractListView(ListView):
     model = Contract
-    template_name = 'crm/contract_list.html'
+    template_name = 'contracts/contracts-list.html'
     context_object_name = 'contracts'
 
 
 class ContractDetailView(DetailView):
     model = Contract
-    template_name = 'crm/contract_detail.html'
+    template_name = 'contracts/contracts-detail.html'
     context_object_name = 'contract'
 
 
 class ContractCreateView(CreateView):
     model = Contract
     form_class = ContractForm
-    template_name = 'crm/contract_form.html'
+    template_name = 'contracts/contracts-create.html'
 
 
 class ContractUpdateView(UpdateView):
     model = Contract
     form_class = ContractForm
-    template_name = 'crm/contract_form.html'
+    template_name = 'contracts/contracts-edit.html'
 
 
 class ContractDeleteView(DeleteView):
     model = Contract
-    template_name = 'crm/contract_confirm_delete.html'
+    template_name = 'contracts/contracts-delete.html'
     success_url = reverse_lazy('crm:contract_list')
 
 
 class CustomerListView(ListView):
     model = Customer
-    template_name = 'crm/customer_list.html'
+    template_name = 'customers/customers-list.html'
     context_object_name = 'customers'
 
 
 class CustomerDetailView(DetailView):
     model = Customer
-    template_name = 'crm/customer_detail.html'
+    template_name = 'customers/customers-detail.html'
     context_object_name = 'customer'
 
 
 class CustomerUpdateView(UpdateView):
     model = Customer
     form_class = CustomerForm
-    template_name = 'crm/customer_form.html'
+    template_name = 'customers/customers-edit.html'
 
 
 class CustomerDeleteView(DeleteView):
     model = Customer
-    template_name = 'crm/customer_confirm_delete.html'
+    template_name = 'customers/customers-delete.html'
     success_url = reverse_lazy('crm:customer_list')
 
 
 class CustomerCreateView(CreateView):
     model = Customer
     form_class = CustomerForm
-    template_name = 'crm/customer_form.html'
+    template_name = 'customers/customers-create.html'
 
     def get_initial(self):
         initial = super().get_initial()
-
-        lead = get_object_or_404(
-            Lead,
-            pk=self.kwargs['lead_pk'],
-        )
-
+        lead = get_object_or_404(Lead, pk=self.kwargs['lead_pk'])
         initial['lead'] = lead
-
         return initial
 
 
 class CampaignStatsView(ListView):
     model = AdvertisingCampaign
-    template_name = 'crm/campaign_stats.html'
-    context_object_name = 'campaigns'
+    template_name = 'ads/ads-statistic.html'
+    context_object_name = 'ads'
 
     def get_queryset(self):
         campaigns = AdvertisingCampaign.objects.annotate(
